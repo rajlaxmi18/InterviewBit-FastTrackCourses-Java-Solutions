1f96e13c7c3e14e8093450f0647e9c0dc2733c8d
Rajlaxmi1018 <72779118+Rajlaxmi1018@users.noreply.github.com>
2020-12-02 00:11:26 +0530

    Add files via upload

---
+++ Basics of Java/Buffered Reader.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        //your code goes here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n1 = Integer.parseInt(reader.readLine());
+        int n2 = Integer.parseInt(reader.readLine());
+        System.out.println(n1 + " " + n2);
+    }
+}
---
+++ Basics of Java/Stdin and Stdout.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+}
